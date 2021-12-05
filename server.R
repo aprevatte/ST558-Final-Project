22a80bdffb6e32c79a3f0df8920785cfbe53f57b
Alex Prevatte <awprevatte@.gmail.com>
2021-12-05 15:28:57 -0500

    Modeling ui update

--- server.R
+++ server.R
@@ -6,6 +6,8 @@ library(DT)
 library(ggrepel)
 library(factoextra)
 library(caret)
+library(tree)
+library(randomForest)
 
 
 #Load data
@@ -63,6 +65,301 @@ function(input, output, session) {
         tab
         })
     
+    #
+        #
+        #
+        # #Model tab data
+         # getModel <- reactive({
+         #     newDataModel <- adultDataSuv %>% filter(position == input$positionSelectModel & year == input$yearsModel) %>% select_if(is.numeric)
+         # }) #end explore tab data
+        # 
+        # #Model tab user inputs
+        # #dependent variable
+        # observe({
+        #     newDataModel <- getDataModel()
+        #     numVars <- newDataModel %>% select_if(is.numeric)
+        #     numVars <- select_if(numVars, colSums(numVars)>0) %>% select(-year) %>% colnames()
+        #     varList <- as.list(numVars)
+        #     updateSelectInput(session, "varDependent", choices = varList, selected = "total_points")
+        # }) #end dependent variable user input
+        # 
+        # #update plot outputs
+         # observe({
+         #     if(input$modelSelect == "Multiple Linear Regression") {
+         #         updateSelectInput(session, "modelPlotSelect", choices = list("Residuals vs Fitted" = 1, "Normal Q-Q" = 2, 
+        #                                                                      "Scale-Location" = 3, "Cook's Distance" = 4, 
+        #                                                                      "Residuals vs Leverage" = 5, "Cook's Dist vs Leverage" = 6))
+        #     } else {
+        #         updateSelectInput(session, "modelPlotSelect", choices = list("Residuals vs Fitted" = 1, "RMSE vs Boosting Iterations" = 2, 
+        #                                                                      "Relative Influence" = 3))
+        #     }
+        #     
+        # }) #end plot outputs
+        # 
+        # #update summary outputs
+        # observe({
+        #     if(input$modelSelect == "Multiple Linear Regression") {
+        #         updateSelectInput(session, "modelSummarySelect", choices = list("Summary"))
+        #     } else {
+        #         updateSelectInput(session, "modelSummarySelect", choices = list("Relative Influence" = 1, "Tune Values" = 2))
+        #     }
+        # })
+        # 
+        # #independent variables
+        # observe({
+        #     newDataModel <- getDataModel()
+        #     numVars <- newDataModel %>% select_if(is.numeric)
+        #     numVars <- select_if(numVars, colSums(numVars)>0) %>% select(-year) %>% colnames()
+        #     varList <- as.list(numVars)
+        #     updateCheckboxGroupInput(session, "varSelectModel", choices = varList, selected = varList[c(1,3:6)])
+        # }) #end independent variable user input
+        # 
+        # #player select
+        # observe({
+        #     newDataModel <- epl %>% filter(position == input$positionSelectModel & year == input$yearsModel)
+        #     
+        #     #update player list
+        #     updateSelectInput(session, "playerSelectModel",
+        #                       choices = newDataModel$full_name %>% sort())
+        # }) 
+        # 
+        # #update players
+        # playerData <- reactive({
+        #     epl %>% filter(full_name == input$playerSelectModel & year == input$yearsModel)
+        # })
+        # 
+        # 
+        # #update player values
+        # observe({
+        #     selectPlayerData <- playerData()
+        #     updateNumericInput(session, "total_points", value = selectPlayerData$total_points)
+        #     updateNumericInput(session, "now_cost", value = as.numeric(selectPlayerData$now_cost))
+        #     updateNumericInput(session, "points_per_game", value = as.numeric(selectPlayerData$points_per_game))
+        #     updateNumericInput(session, "minutes", value = as.numeric(selectPlayerData$minutes))
+        #     updateNumericInput(session, "goals_scored", value = as.numeric(selectPlayerData$goals_scored))
+        #     updateNumericInput(session, "assists", value = as.numeric(selectPlayerData$assists))
+        #     updateNumericInput(session, "bonus", value = as.numeric(selectPlayerData$bonus))
+        #     updateNumericInput(session, "bps", value = as.numeric(selectPlayerData$bps))
+        #     updateNumericInput(session, "goals_conceded", value = as.numeric(selectPlayerData$goals_conceded))
+        #     updateNumericInput(session, "own_goals", value = as.numeric(selectPlayerData$own_goals))
+        #     updateNumericInput(session, "penalties_missed", value = as.numeric(selectPlayerData$penalties_missed))
+        #     updateNumericInput(session, "yellow_cards", value = as.numeric(selectPlayerData$yellow_cards))
+        #     updateNumericInput(session, "red_cards", value = as.numeric(selectPlayerData$red_cards))
+        #     updateNumericInput(session, "saves", value = as.numeric(selectPlayerData$saves))
+        #     updateNumericInput(session, "clean_sheets", value = as.numeric(selectPlayerData$clean_sheets))
+        #     updateNumericInput(session, "creativity", value = as.numeric(selectPlayerData$creativity))
+        #     updateNumericInput(session, "ict_index", value = as.numeric(selectPlayerData$ict_index))
+        #     updateNumericInput(session, "influence", value = as.numeric(selectPlayerData$influence))
+        #     updateNumericInput(session, "threat", value = as.numeric(selectPlayerData$threat))
+        #     updateNumericInput(session, "selected_by_percent", value = as.numeric(selectPlayerData$selected_by_percent))
+        # })
+        # 
+        # #end model tab user inputs
+        # 
+        # #training data set
+        # train <- reactive({
+        #     
+        #     #get data
+        #     dataModel <- getDataModel()
+        #     
+        #     #set seed prior to sampling
+        #     set.seed(1)
+        #     
+        #     #sample observations for training data set
+        #     train <- sample(1:nrow(dataModel), size = nrow(dataModel)*(as.numeric(input$train)/100))
+        #     
+        #     #use remaining samples for test data set
+        #     test <- dplyr::setdiff(1:nrow(dataModel), train)
+        #     
+        #     return(list(train,test))
+        #     
+        # }) #end training data set
+        # 
+        # #Plot output
+        # output$Modelplot <- renderPlot(
+        #     if(input$modelSelect == "Multiple Linear Regression"){
+        #         plot(plotModel()[[1]], which = as.numeric(input$modelPlotSelect), main = "Multiple Linear Regression")
+        #     } else if(input$modelPlotSelect == 1) {
+        #         fitModel <- plotModel()[[1]]
+        #         predictValues <- predict(fitModel)
+        #         actualValues <- fitModel$finalModel$data$y
+        #         plot(predictValues-actualValues, ylab = "Residuals", xlab = "Fitted Values", main = "Boosted Trees Model: Residuals vs Fitted Values"); abline(h=0, lty=2)
+        #     } else if(input$modelPlotSelect == 2) {
+        #         plot(plotModel()[[1]], main = "Boosted Trees Model: RMSE vs # Boosting Iterations")
+        #     } else {
+        #         plot(summary(plotModel()[[1]]), main = "Boosted Trees Model: Relative Influence")
+        #     }
+        # ) #end plot output
+        # 
+        # #Model tab plots
+        # plotModel <- reactive({
+        #     
+        #     #get value of user inputs
+        #     center <- input$centerModel
+        #     
+        #     #get filtered data
+        #     newDataModel <- getDataModel()
+        #     
+        #     #get test train
+        #     train_test <- train()
+        #     train <- train_test[[1]]
+        #     test <- train_test[[2]]
+        #     
+        #     #create separate training and test data frames
+        #     dfTrain <- newDataModel[train, ]
+        #     dfTest <- newDataModel[test, ]
+        #     
+        #     #formula step 1
+        #     if(input$varInteract == 1 & input$modelSelect == "Multiple Linear Regression") {
+        #         independent <- paste(input$varSelectModel, collapse = "*")
+        #     } else {
+        #         independent <- paste(input$varSelectModel, collapse = "+")
+        #     }
+        #     
+        #     #formula step 2
+        #     fm <- paste(input$varDependent,"~",independent)
+        #     
+        #     #cross-validation
+        #     trctrl <- trainControl(method = "repeatedcv", number = input$folds, repeats = input$repeats)
+        #     
+        #     #tuning grid
+        #     gbmGrid <-  expand.grid(interaction.depth = c(1, 5, 9), 
+        #                             n.trees = input$numTrees, 
+        #                             shrinkage = 0.1,
+        #                             n.minobsinnode = 20
+        #     )
+        #     
+        #     #fit models
+        #     if(input$modelSelect == "Multiple Linear Regression") {
+        #         #linear model
+        #         fit <- lm(formula = as.formula(fm), data = as.data.frame(dfTrain))
+        #     } else if(input$CV == 1){
+        #         if(input$trees == 1) {
+        #             fit <- caret::train(as.formula(fm), data = as.data.frame(dfTrain), method = "gbm", trControl = trctrl, verbose = FALSE, tuneGrid = gbmGrid)
+        #         } else {
+        #             fit <- caret::train(as.formula(fm), data = as.data.frame(dfTrain), method = "gbm", trControl = trctrl, verbose = FALSE)
+        #         }
+        #     } else {
+        #         if(input$trees == 1) {
+        #             fit <- caret::train(as.formula(fm), data = as.data.frame(dfTrain), method = "gbm", verbose = FALSE, tuneGrid = gbmGrid)
+        #         } else {
+        #             fit <- caret::train(as.formula(fm), data = as.data.frame(dfTrain), method = "gbm", verbose = FALSE)
+        #         }    
+        #     }
+        #     
+        #     return(list(fit, dfTest, dfTrain))
+        #     
+        # }) #end model plots reactive
+        # 
+        # #RMSE output
+        # output$predictModel <- renderPrint({
+        #     dfTest <- plotModel()[[2]]
+        #     dependent <- as.data.frame(dfTest) %>% select(input$varDependent)
+        #     predict <- predict(plotModel()[[1]], newdata = as.data.frame(dfTest))
+        #     error <- unlist(predict-dependent)
+        #     sqrt(mean((error)^2))
+        # }) #end RMSE output
+        # 
+        # #summary output
+        # output$summaryModel <- renderPrint({
+        #     if(input$modelSelect == "Multiple Linear Regression"){
+        #         summary(plotModel()[[1]])
+        #     } else if(input$modelSummarySelect == 1) {
+        #         summary(plotModel()[[1]])
+        #     } else {
+        #         fit <- plotModel()[[1]]
+        #         fit$finalModel$tuneValue
+        #     }
+        #     
+        # }) #end summary output
+        # 
+        # #predict value output
+        # observe({
+        #     #   #var dependent
+        #     output$predictInfo <- renderText({
+        #         paste0("Prediction for ", input$varDependent)
+        #     })
+        #     
+        #     output$predictValue <- renderPrint({
+        #         total_points <- input$total_points
+        #         now_cost <- input$now_cost
+        #         points_per_game <- input$points_per_game
+        #         minutes <- input$minutes
+        #         goals_scored <- input$goals_scored
+        #         assists <- input$assists
+        #         bonus <- input$bonus
+        #         bps <- input$bps
+        #         goals_conceded <- input$goals_conceded
+        #         own_goals <- input$own_goals
+        #         penalties_missed <- input$penalties_missed
+        #         penalties_saved <- input$penalties_saved
+        #         yellow_cards <- input$yellow_cards
+        #         red_cards <- input$red_cards
+        #         saves <- input$saves
+        #         clean_sheets <- input$clean_sheets
+        #         creativity <- input$creativity
+        #         ict_index <- input$ict_index
+        #         influence <- input$influence
+        #         threat <- input$threat
+        #         selected_by_percent <- input$selected_by_percent
+        #         
+        #         newData <- as.data.frame(cbind(total_points, now_cost, points_per_game, minutes, goals_scored, assists, bonus, bps, goals_conceded, own_goals, 
+        #                                        penalties_missed, penalties_saved, yellow_cards, red_cards, saves, clean_sheets, creativity, ict_index, influence, threat, selected_by_percent))
+        #         
+        #         fit<-plotModel()[[1]]
+        #         
+        #         prediction <- predict(fit, newData)
+        #         as.vector(prediction)
+        #     }) #end predict value output
+        # })
+        # 
+        # 
+        # #plot download handler
+        # output$downloadModelPlot <- downloadHandler(
+        #     filename = function() {
+        #         paste("EPL", input$modelSelect, input$positionSelectModel, input$yearsModel, ".png", sep ="_")
+        #     },
+        #     content = function(file) {
+        #         png(file, width = 1200)
+        #         if(input$modelSelect == "Multiple Linear Regression"){
+        #             plot(plotModel()[[1]], which = as.numeric(input$modelPlotSelect), main = "Multiple Linear Regression")
+        #         } else if(input$modelPlotSelect == 1) {
+        #             fitModel <- plotModel()[[1]]
+        #             predictValues <- predict(fitModel)
+        #             actualValues <- fitModel$finalModel$data$y
+        #             plot(predictValues-actualValues, ylab = "Residuals", xlab = "Fitted Values", main = "Boosted Trees Model: Residuals vs Fitted Values"); abline(h=0, lty=2)
+        #         } else if(input$modelPlotSelect == 2) {
+        #             plot(plotModel()[[1]], main = "Boosted Trees Model: RMSE vs # Boosting Iterations")
+        #         } else {
+        #             plot(summary(plotModel()[[1]]), main = "Boosted Trees Model: Relative Influence")
+        #         }
+        #         dev.off()
+        #     }
+        # ) #end plot download handler
+        # 
+        # #data download handler (training data)
+        # output$downloadModelData <- downloadHandler(
+        #     filename = function() {
+        #         paste0("EPL_", input$modelSelect, "_training_data.csv")
+        #     },
+        #     content = function(file) {
+        #         write.csv(plotModel()[[3]], file, row.names = FALSE)
+        #     }
+        # ) #end data download handler
+        # 
+        # #data download handler (testing data)
+        # output$downloadTestData <- downloadHandler(
+        #     filename = function() {
+        #         paste0("EPL_", input$modelSelect, "_testing_data.csv")
+        #     },
+        #     content = function(file) {
+        #         write.csv(plotModel()[[2]], file, row.names = FALSE)
+        #     }
+        # ) #end data download handler
+        # 
+        # # #end model tab
+        # 
+        # ____________________________________________________________________________________________________________________
     
     # Data Tab 
     getData <- reactive({
@@ -70,6 +367,7 @@ function(input, output, session) {
                                             Sex == input$sexChoice) %>% 
             select(as.vector(input$variableChoice))
     }) 
+    
     
     #Data tab output
     output$Table <- renderDataTable({
--- ui.R
+++ ui.R
@@ -79,26 +79,118 @@ dashboardPage(
         )
                     )),
         
-        # # Data table tab
-        # tabItem(tabName = "data",
-        #         fluidRow(
-        #             column(3,
-        #                    box(width = 12,
-        #                        selectInput("incomeChoice", label = "Income", 
-        #                                    choices = list("<=50K" = "<=50K", ">50K" = ">50K")),
-        #                        selectInput("sexChoice", label = "Gender", 
-        #                                    choices = list("Male" = "Male", "Female" = "Female")),
-        #                        checkboxGroupInput("variableChoice", label = "Variable Choice",
-        #                                           choices = list("Age", "Workclass", "fnlwgt", "Education", "Education_Number", "Marital_Status", "Occupation", "Relationship", "Race", "Capital_Gain", "Capital_Loss", "Native_Country"),
-        #                                           selected = list("Age", "Workclass", "Education"))
-        #                    )
-        #             ),
-        #             column(9, 
-        #                    dataTableOutput("Table"),
-        #                    downloadButton("downloadData", "Download")
-        #             )
-        #         )
-        # ),
+        #
+        #
+        #
+        #Start models tab
+        tabItem(tabName = "modeling",
+                tabsetPanel(
+                    tabPanel("Info",
+                              fluidRow(
+                                  h2("Modeling Approaches"),
+                                  h3("Generalized Linear Regression - Logistic Regression"),
+                                  h4("Logistic regression measures the relationship between the dependent and independent variables by estimating probabilities of success using the logit function. The benefits of this approach is that it is easy to implement and efficient to train. The disadvantage to this modeling technique is that it should not be used if the number of observations is less than the number of predictor variables. Another disadvantage is the assumption of linearity between the response and the predictors."),
+                                  h3("Classification Tree"),
+                                  h4("Classification trees are used for assigning a variable to a categorical class. The algorithm then identifies the class in which the target variable would fall under. The advantages to this procedure is that normalization and scaling are not required. Also, interpretation through tree visualizations can be straight forward. The disadvantage to this method is that small changes in the data can dramatically change the structure of the tree. In addition, there is a high probability of over-fitting."),
+                                  h3("Random Forest Model"),
+                                  h4("The random forest model is used to build an ensemble of decision trees and average them together. The advantage of this method is that due to averaging and randomization, this model is less likely to overfit the data. The disadvantage to this technique is the complexity of combining many trees and how this relates to interpretability. The training time for a random forest model can be much longer compared to regular decision trees."),
+                                  h3("Equation to evaluate model performance"),
+                                  h4(withMathJax(helpText("$$Accuracy = {\\frac{TP+TN}{TP+TN+FP+FN}}$$"))),
+                              )),
+        
+        tabPanel("Fitting",
+                 fluidRow(
+                     column(4,
+            box(width = 12, title = "Model Parameters",
+               selectInput("variables", label = "Predictor variables",
+                                        choices = list("Age" = "AGE", "Education_Number" = "EDNUM", "Marital_Status" = "MS", "Relationship" = "RELATIONSHIP", "Sex" = "SEX", "Hours_per_week" = "HPW", "Income" = "INCOME")),
+               
+               selectInput("modelChoice", label = "Model Type",                                                choices = list("Logistic Regression", "Classification Tree", "Random Forest")),
+                 sliderInput("train", "Percent of Training Data to Partition",
+                             min = 20, max = 100, step = 5, value = 70),
+                 # conditionalPanel(condition = "input.modelSelect == 'Logistic Regression'"),
+                 # conditionalPanel(condition = "input.modelSelect == 'Classification Tree'"),
+                 # conditionalPanel(condition = "input.modelSelect == 'Random Forest'",
+                 # checkboxInput("CV", "Cross Validation", value = FALSE)),
+                
+                 # conditionalPanel(condition = "input.CV == 1",
+                 # sliderInput("folds", "Folds", min = 2, max = 10, value = 10),
+                 # sliderInput("repeats", "Repeats", min = 2, max = 10, value = 3)
+                 #                                 ),
+                 checkboxInput("trees", tags$b("Select Trees"), value = FALSE),
+                 conditionalPanel(condition = "input.trees == 1",
+                 sliderInput("numTrees", "Trees", min = 50, max = 1000, step = 50, value = 500)
+                                                 )
+                                            ),
+                                box(width = 12, title = "Output",
+                                selectInput("modelSummary", "Summary",
+                                                         choices = NULL),
+                                selectInput("modelSummarySelect", "Summary",
+                                                         choices = NULL)
+                                         ),
+                                 column(9,
+                                         box(width = 14,
+                                             plotOutput("Modelplot", height = 500),
+                                             downloadButton("downloadModelPlot", "Plot"),
+                                             downloadButton("downloadModelData", "Training Data"),
+                                             downloadButton("downloadTestData", "Testing Data")
+                                         ))))),
+        #                                 h4("Testing Data RMSE"),
+        #                                 verbatimTextOutput(("predictModel")),
+        #                                 h4("Model Summary"),
+        #                                 verbatimTextOutput(("summaryModel"))
+        #                          )
+        #                      ) #end fluidRow
+        #             ), #end tabPanel "Model"
+    
+        
+                     tabPanel("Prediction"))),
+                              
+        #                          column(3,
+        #                                 box(width = 12, title = "Select Player or Enter Values",
+        #                                     # actionButton("predictButton", label = "Predict"),
+        #                                     # br(),
+        #                                     # br(),
+        #                                     selectInput("playerSelectModel", label = "Player", 
+        #                                                 choices = NULL),
+        #                                     numericInput("total_points","total_points", value = NULL), #total_points
+        #                                     numericInput("now_cost","now_cost", value = NULL), #now_cost
+        #                                     numericInput("points_per_game","points_per_game", value = NULL), #points_per_game
+        #                                     numericInput("minutes","minutes", value = NULL), #minutes
+        #                                     numericInput("goals_scored","goals_scored", value = NULL), #goals_scored
+        #                                     numericInput("assists","assists", value = NULL), #assists
+        #                                     numericInput("bonus","bonus", value = NULL), #bonus
+        #                                     numericInput("bps","bps", value = NULL), #bps
+        #                                     numericInput("goals_conceded","goals_conceded", value = NULL), #goals_conceded
+        #                                     numericInput("own_goals","own_goals", value = NULL), #own_goals
+        #                                     numericInput("penalties_missed","penalties_missed", value = NULL), #penalties_missed
+        #                                     numericInput("penalties_saved","penalties_saved", value = NULL), #penalties_saved
+        #                                     numericInput("yellow_cards","yellow_cards", value = NULL), #yellow_cards
+        #                                     numericInput("red_cards","red_cards", value = NULL), #red_cards
+        #                                     numericInput("saves","saves", value = NULL), #saves
+        #                                     numericInput("clean_sheets","clean_sheets", value = NULL), #clean_sheets
+        #                                     numericInput("creativity","creativity", value = NULL), #creativity
+        #                                     numericInput("ict_index","ict_index", value = NULL), #ict_index
+        #                                     numericInput("influence","influence", value = NULL), #influence
+        #                                     numericInput("threat","threat", value = NULL), #threat
+        #                                     numericInput("selected_by_percent","selected_by_percent", value = NULL) #threat
+        #                                 ) #end box
+        #                                 
+        #                          ), #end column1
+        #                          column(3,
+        #                                 box(width = 12,
+        #                                     textOutput("predictInfo"),
+        #                                     verbatimTextOutput("predictValue")
+        #                                 ) #end box
+        #                          ) #end column2
+        #                          
+        #                      ) #end fluidRow
+        #             ) #end tabPanel "Predict"
+        #             
+        #         ) #end tabsetPanel
+        #         
+        # ), #end model tab
+   
         
         
         
